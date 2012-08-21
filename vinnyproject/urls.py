2dc497a05b94935fadc842ce3586035e254b213c
cocbiz <cocbiz@gmail.com>
2012-08-21 18:17:02 +0700

    Serve static files temporarily

--- vinnyproject/urls.py
+++ vinnyproject/urls.py
@@ -8,9 +8,16 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^$', direct_to_template, {'template': 'homepage.html'}, name='home'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^payment/', include('payment.urls')),
     url(r'^paypal/ipn/', include('paypal.standard.ipn.urls')),
     url(r'^paypal/pdt/', include('paypal.standard.pdt.urls')),
 )
+
+# Should remove if static files served separately (e.g. S3, etc.)
+from django.conf import settings
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}), 
+)   
